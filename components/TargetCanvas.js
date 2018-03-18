c424a4f33930c12bfe6868efe3d428816d72f861
Carles Andres <carlos.andres.c@gmail.com>
2018-03-18 22:59:13 +0000

    Basic PoC

---
+++ components/TargetCanvas.js
@@ -0,0 +1,43 @@
+import pica from 'pica';
+
+class TargetCanvas extends React.Component {
+  componentDidMount() {
+    this.redraw();
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.redraw();
+  }
+
+  redraw() {
+    const { image, size } = this.props;
+    if (!image) {
+      return;
+    }
+
+    console.log('image', image);
+    console.log('size', size);
+
+    const ctx = this.target.getContext('2d');
+    ctx.drawImage(image, 0, 0, size, size);
+  }
+
+  render() {
+    const { size } = this.props;
+
+    return (
+      <div className="preview">
+        <div>Size: {size}px x {size}px</div>
+
+        <canvas
+          className="target"
+          width={size}
+          height={size}
+          ref={ ref => { this.target = ref; } }
+        />
+      </div>
+    );
+  }
+};
+
+export default TargetCanvas;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
+import TargetCanvas from '../components/TargetCanvas';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { image: null };
+    this.targetCanvases = [];
     this.onChange = this.onChange.bind(this);
-    this.drawOnCanvas = this.drawOnCanvas.bind(this);
-    this.state = { image : null };
+    this.afterLoad = this.afterLoad.bind(this);
   }
 
   componentDidMount() {
-    this.setState({ image: (new Image()) },
-      () => { this.state.image.onload = this.drawOnCanvas; });
+    const image = new Image();
+    image.onload = this.afterLoad;
+    this.setState({ image });
   }
 
   onChange(event) {
@@ -17,10 +21,27 @@ class App extends React.Component {
     this.state.image.src = URL.createObjectURL(file);
   }
 
-  drawOnCanvas() {
-    window.URL.revokeObjectURL(this.state.image);
-    const ctx = this.canvas.getContext('2d');
-    ctx.drawImage(this.state.image, 0, 0);
+  afterLoad() {
+    console.log('after load');
+    if (this.state.image.src) {
+      window.URL.revokeObjectURL(this.state.image.src);
+    }
+    this.forceUpdate();
+  }
+
+  getTargetCanvases() {
+    const { image } = this.state;
+    if (!image || !image.src) {
+      return null;
+    }
+
+    const sizes = [32, 64, 128];
+
+    const canvases = sizes.map( size =>
+        <TargetCanvas size={size} key={`${size}`} image={image} />
+    );
+
+    return canvases;
   }
 
   render() {
@@ -30,20 +51,26 @@ class App extends React.Component {
           input {
             display: block;
           }
-        `}
-        </style>
-        <h1>IconFist</h1>
-        <h2>Create favicons quickly</h2>
-        <input
-          type="file"
-          onChange={this.onChange}
-          accept="image/*" />
-        <canvas
-          ref={ (ref) => { this.canvas = ref;} }
-          width="300"
-          height="300"></canvas>
 
-      </div>
+          canvas {
+            background: white;
+          }
+        `}
+      </style>
+      <h1>IconFist</h1>
+      <h2>Create favicons quickly</h2>
+      <input
+        type="file"
+        onChange={this.onChange}
+        accept="image/*" />
+      {/* <canvas */}
+        {/*   className="origin" */}
+        {/*   ref={ (ref) => { this.canvas = ref;} } */}
+        {/*   width="300" */}
+        {/*   height="300"></canvas> */}
+      {this.getTargetCanvases()}
+
+    </div>
     );
   }
 };
